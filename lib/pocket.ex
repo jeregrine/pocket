a454938c07e24d5a205a8356764b0f614d2efd7b
Jason Stiebs <jason@fly.io>
2026-09-25 15:21:27 -0500

    make it so

---
+++ lib/pocket.ex
@@ -0,0 +1,18 @@
+defmodule Pocket do
+  @moduledoc """
+  Documentation for `Pocket`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pocket.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
